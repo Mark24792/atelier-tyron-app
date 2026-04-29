424ff8b160111b859c2b100e047514bb9463a388
Mark24792 <Mark-biedermann@web.de>
2026-04-29 15:16:44 +0200

    Create auth.login.tsx

---
+++ app/routes/auth.login.tsx
@@ -0,0 +1,6 @@
+import { login } from "../shopify.server";
+import { type LoaderFunctionArgs } from "@remix-run/node";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  return login(request);
+};
