6ded500bd73e0c2a5e729e388b73fc588cf44201
Mark24792 <mark-biedermann24792@web.de>
2026-04-29 19:58:04 +0200

    add type module

--- build/server/index.js
+++ build/server/index.js
@@ -143,10 +143,8 @@ const shopify = shopifyApp({
   authPathPrefix: "/auth",
   distribution: AppDistribution.AppStore,
   sessionStorage: new PrismaSessionStorage(db),
-  future: {},
-  cookieOptions: {
-    sameSite: "none",
-    secure: true
+  future: {
+    unstable_newEmbeddedAuthStrategy: true
   },
   hooks: {
     afterAuth: async ({ session }) => {
@@ -206,18 +204,31 @@ const route1 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProper
 }, Symbol.toStringTag, { value: "Module" }));
 const loader$7 = async ({ request }) => {
   const { searchParams } = new URL(request.url);
+  const exitIframe = searchParams.get("exitIframe");
   const shop = searchParams.get("shop");
   const host = searchParams.get("host");
-  const exitIframe = searchParams.get("exitIframe");
-  if (exitIframe) {
-    const destination = new URL(exitIframe, `https://${shop}`);
-    if (host) destination.searchParams.set("host", host);
-    return new Response(null, {
-      status: 302,
-      headers: { Location: destination.toString() }
-    });
-  }
-  return new Response(null, { status: 200 });
+  if (!exitIframe) return new Response(null, { status: 200 });
+  const destination = exitIframe.startsWith("http") ? exitIframe : `https://${shop}${exitIframe}`;
+  return new Response(
+    `<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"><\/script>
+  <script>
+    var AppBridge = window['app-bridge'];
+    var createApp = AppBridge.default;
+    var Redirect = AppBridge.actions.Redirect;
+    var app = createApp({ apiKey: '155b7606b434f4003fdd811236a1b064', host: '${host}' });
+    Redirect.create(app).dispatch(Redirect.Action.REMOTE, '${destination}');
+  <\/script>
+</head>
+<body></body>
+</html>`,
+    {
+      status: 200,
+      headers: { "Content-Type": "text/html" }
+    }
+  );
 };
 const route2 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
